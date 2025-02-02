3c19569065860b9270ce0253b97def72abcf9473
J. Dale <dale.gonzalez@thoughtbox.ai>
2025-02-02 11:03:04 -0500

    Minor start to adding a web-front

--- server.py
+++ server.py
@@ -5,10 +5,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "Word"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+def read_root(q: str):
+    return {"query": q}
